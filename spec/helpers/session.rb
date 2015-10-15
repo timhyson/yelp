63a4de1135f61b3ab19610ae004bd248d02834b1
Tim Hyson <tjhyson@gmail.com>
2015-10-15 12:10:02 +0100

    user cannot edit restaurants it did not create

--- spec/helpers/session.rb
+++ spec/helpers/session.rb
@@ -6,4 +6,11 @@ module SessionHelpers
      fill_in :user_password_confirmation, with: user.password_confirmation
      click_button 'Sign up'
    end
+
+   def sign_in(user)
+      visit '/users/sign_in'
+      fill_in :Email,                      with: user.email
+      fill_in :Password,                 with: user.password
+      click_button 'Log in'
+    end
  end
