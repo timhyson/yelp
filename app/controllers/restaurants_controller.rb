0c02750b008cbf61cf72b83120f828618921a971
Tim Hyson <tjhyson@gmail.com>
2015-10-17 13:34:30 +0100

    reviewer can add a photo to their reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -19,7 +19,7 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name)
+    params.require(:restaurant).permit(:name, :image)
   end
 
   def show
